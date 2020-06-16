6b0eafaa91f4caeb300f1e65a852fe15c592e293
mio-eldar <eldar.djorobekov@gmail.com>
2020-06-16 18:16:37 +0600

    static files configured

--- src/src/settings.py
+++ src/src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@up$2bc20x+z2xp0ui%_tl!wogmiw$lop+xc$lph-l2+zlcl^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "main/main_static"),
+    os.path.join(BASE_DIR, "main/static_files"),
 ]
 
 
