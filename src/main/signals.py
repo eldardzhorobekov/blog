21643d9ba32efaac1b8e53067eec77840775393d
mio-eldar <eldar.djorobekov@gmail.com>
2020-06-22 17:06:18 +0600

    Теперь сообщения при создании нового поста отправляются асинхронно с помощью потоков

--- src/main/signals.py
+++ src/main/signals.py
@@ -1,26 +1,20 @@
-from django.core.signals import request_finished
-from django.core.mail import send_mail
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 from main.models import Post
+from main.utils import send_mail_async
 
 
 @receiver(post_save, sender=Post)
 def send_mail_when_post_created(sender, instance, created, **kwargs):
     if created:
         subject = 'Новый пост в вашей ленте!'
-        message = 'Автор <%s> создал новый пост: %s' % (
-            instance.author, instance.title)
-        from_email = settings.EMAIL_HOST_USER
+        link = reverse('post-details', kwargs={'pk': 3})
+        html_content = 'Автор <b>%s</b> создал новый пост: ' \
+                       '<a href="%s">%s</a>' % (
+                            instance.author,
+                            link,
+                            instance.title)
         recievers = instance.author.subscriptions.values_list(
             'email', flat=True)
-        if recievers.count() > 0:
-            send_mail(
-                subject, message, from_email, recievers, fail_silently=False
-            )
-
-
-@receiver(request_finished)
-def my_callback(sender, **kwargs):
-    print('Request finished!')
+        send_mail_async(subject, html_content, recievers)
