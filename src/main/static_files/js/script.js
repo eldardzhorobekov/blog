a4cf5653960d35f5142dae52c42b9e89317eff54
mio-eldar <eldar.djorobekov@gmail.com>
2020-07-12 15:13:25 +0600

    Add follow list templates. Code rewrite: follow_btn (in script.js)

--- src/main/static_files/js/script.js
+++ src/main/static_files/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  const
-    markBtn = $('.mark-btn'),
-    followBtn = $('.follow-btn');
+  // Initalize materialize Modal component
+  $('.modal').modal();
+
+  const markBtn = $('.mark-btn');
+  const followBtn = $('.follow__btn');
 
   $(markBtn).on('click', function() {
     const icon = $(this).children('i');
@@ -20,37 +22,77 @@ $(document).ready(function () {
   });
 
   const FOLLOWED = 'followed';
+  const FOLLOWED_HTML = 'Following';
   const UNFOLLOWED = 'unfollowed';
+  const UNFOLLOWED_HTML = 'Follow';
+
+  function toggleFollowBtn(btn) {
+    // Toggle class and html value of button
+
+    const is_following = $(btn).hasClass(FOLLOWED);
+    
+    $(btn).removeClass(UNFOLLOWED);
+    $(btn).removeClass(FOLLOWED);
+
+    if(is_following) {
+      $(btn).addClass(UNFOLLOWED);
+      $(btn).html(UNFOLLOWED_HTML);
+    } else {
+      $(btn).addClass(FOLLOWED);
+      $(btn).html(FOLLOWED_HTML);
+    }
+  }
 
   $(followBtn).on('click', function() {
-    const state = $(this).hasClass(FOLLOWED) ? FOLLOWED : UNFOLLOWED;
+    // This function makes AJAX request to server to make subscription
+    // and toggles the button's class and html value
+    const that = this;
+    const is_following = $(this).hasClass(FOLLOWED);
     const username = $(this).attr('data-username');
-    // console.log(state, username, $(this));
-
-    let url, class_remove, class_add;
-
-    if(state === UNFOLLOWED) {
-      url = `/profile/${username}/follow`;
-      class_remove = UNFOLLOWED;
-      class_add = FOLLOWED;
-      html = 'Following';
-    } else {
-      url = `/profile/${username}/unfollow`;
-      class_remove = FOLLOWED;
-      class_add = UNFOLLOWED;
-      html = 'Follow';
-    }
-
-
-    $.post(url, () => {
-      $(this).removeClass(class_remove);
-      $(this).addClass(class_add);
-      $(this).html(html);
-    });
-
+    const url = `/profile/${username}/` + (is_following ? 'unfollow' : 'follow');
+    $
+      .post(url, () => {
+        // console.log('Sending');
+      })
+      .done(() => {
+        toggleFollowBtn(that);
+        // console.log('Success!');
+      })
+      .fail(() => {
+        console.error('No subscription');
+      });
   });
 
 
-  $('.modal').modal();
+  function generateFollowItem(username='username', is_following=true) {
+    // This function creates:
+    // <li class="follow__item">
+    //   <a href="#" class="material-icon__wrapper">
+    //     <i class="material-icons">person</i>
+    //     <span class="black-text">{{ username }}</span>
+    //   </a>
+    //   <a href="#" class="follow__btn followed" data-username="{{ username }}">Following</a>
+    // </li>
 
+    // Toggles button if is_following == false
+
+    const parent_li = $('<li/>').addClass('follow__item');
+    const user_a = $('<a/>').attr('href', '#').addClass('material-icon__wrapper follow__user').appendTo(parent_li);
+    const user_icon_i = $('<i/>').addClass('material-icons').html('person').appendTo(user_a);
+    const user_name_span = $('<span/>').html(username).addClass('black-text').appendTo(user_a);
+    const follow_btn = $('<a/>').attr('href','#').addClass('follow__btn '+FOLLOWED).attr('data-username', username).html(FOLLOWED_HTML).appendTo(parent_li);
+    
+    if(!is_following) toggleFollowBtn(follow_btn);
+
+    return parent_li;
+  }
+
+  const followers_list = $('.follow__followers_list');
+  // for testing purpose
+  for(let i=0; i < 10; ++i) {
+    generateFollowItem(username='Followed user', is_following=true).appendTo(followers_list);
+  }
+  for(let i=0; i < 5; ++i) {
+    generateFollowItem(username='Unfollowed user', is_following=false).appendTo(followers_list);
+  }
 });
